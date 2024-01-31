3621b3037b815c02936a7e19f79351eb2f31ec7f
stfilter <123819448+stfilter@users.noreply.github.com>
2024-01-31 22:42:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://vaathi-8j1f.onrender.com/".format(FQDN)
+    URL = "https://st-thamizhan.onrender.com/".format(FQDN)
 else:
-    URL = "https://vaathi-8j1f.onrender.com/".format(FQDN)
+    URL = "https://st-thamizhan.onrender.com/".format(FQDN)
 
 
 
