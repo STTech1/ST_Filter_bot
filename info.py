7110feb87a433128cffcd2f1c6bf141b5026a6d4
stfilter <123819448+stfilter@users.noreply.github.com>
2024-02-06 10:45:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '435ee53e67759c990f025df73c3510d0ea3e048e')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'kingurl.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '7ae3c13f836145f7f5ada9c8b651c17d46448b96')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
